180d39ba59e93278053d89dc379f2bbff8fac918
Almir Rogério Camolesi <camolesi@femanet.com.br>
2017-05-20 12:12:00 -0300

    Projeto Locacao

---
+++ Locacao/Locacao/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Locacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Camadas.Model.Cliente cli = new Camadas.Model.Cliente();
+            cli.Id = 10; 
+        }
+    }
+}
